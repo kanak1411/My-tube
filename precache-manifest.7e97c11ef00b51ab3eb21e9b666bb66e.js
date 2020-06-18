75f57d937673f1eb76a82015836ad2375979defd
kanak1411 <kanak1411@gmail.com>
2020-06-18 15:31:23 +0530

    Updates

---
+++ precache-manifest.7e97c11ef00b51ab3eb21e9b666bb66e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba3cc4d9affd2d5cd648ce05e7e5066",
+    "url": "/My-tube/index.html"
+  },
+  {
+    "revision": "6256286c380689b33fad",
+    "url": "/My-tube/static/css/main.2b32f5dd.chunk.css"
+  },
+  {
+    "revision": "45aa4ed82cb489b4b333",
+    "url": "/My-tube/static/js/2.e54b287c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-tube/static/js/2.e54b287c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6256286c380689b33fad",
+    "url": "/My-tube/static/js/main.3f3e20e2.chunk.js"
+  },
+  {
+    "revision": "05ded1e95cc9bddf27e5",
+    "url": "/My-tube/static/js/runtime-main.00343ac5.js"
+  }
+]);
